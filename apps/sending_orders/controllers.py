00beeae19d12fea6a3885e6450bcb7d11eea6d7b
Alex Rudkovskiy <xandr.rudkovsky@gmail.com>
2020-07-18 22:51:06 +0300

    C5. Add comments in sending_orders/controllers.py for Alexander B.

--- apps/sending_orders/controllers.py
+++ apps/sending_orders/controllers.py
@@ -7,17 +7,28 @@ from flask import (
 )
 
 
+# здесь создаем blueprint. Его потом подключаем в файле manage.py в корне
+# название senor, префикс /senor, т.е. в приложении все роуты к нему будут обрабатываться как /senor/*
+# , но в роутах этого модуля префикс опускается
 senor = Blueprint('senor', __name__, url_prefix='/senor', static_folder='static', template_folder='templates')
 
 
+# отрисовываю страницу, она доступна по ссылке http://localhost:5000/senor/
 @senor.route('/')
 def index():
     return render_template('sending_orders.html', title='Название страницы')
 
 
+# функция для отлавливания запросов со страницы, те которые отправляются функцией fetch
 @senor.route('/run_script', methods=['POST'])
 def run_script():
+    # получаем содержимое запроса
     req = request.get_json()
+
+    # содержимое запроса используется в запуске функции. Функцию нужно импортировать и запустить ниже
+    # your code is here
+
+    # формируем ответ для отрисовки на странице
     res = make_response(jsonify('Все ок'), 200)
 
     return res
